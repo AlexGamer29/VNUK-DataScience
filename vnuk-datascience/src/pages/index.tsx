4316f54f0077c9e84ea0de195506a71b5459eaf6
[TUF] AlexGamer29 <sun123vet@gmail.com>
2022-07-29 23:45:32 +0700

    [feat] complete Home page

--- vnuk-datascience/src/pages/index.tsx
+++ vnuk-datascience/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import MainLayout from "@src/layouts/main";
 import styles from "@/styles/Home.module.css";
-import { sanityClient, urlFor } from "../config/sanity"
+import { sanityClient, urlFor } from "../config/sanity";
 import { Post } from "../config/typings";
 import Link from "next/link";
+import HeaderImage from "../../public/assets/images/header.jpg";
 
 interface Props {
-  posts: [Post],
+  posts: [Post];
 }
 
 const Home: NextPage = ({ posts }: Props) => {
@@ -15,29 +17,80 @@ const Home: NextPage = ({ posts }: Props) => {
   return (
     <MainLayout>
       <div className={styles.container}>
+        <div className="flex w-full flex-col items-center py-10">
+          <Image src={HeaderImage} className="h-full object-contain"></Image>
+        </div>
+        <div className="flex w-full flex-col items-center p-10">
+          <h1 className="text-4xl w-1/2 font-bold text-center text-vnuk-red pb-5">
+            A few words about this blog platform, VNUK Data Science, and how
+            this site was made
+          </h1>
+          <h1 className="text-xl font-normal text-center text-vnuk-blue">
+            Why you choose instead of other blog websites
+          </h1>
+        </div>
+        <div className="flex flex-col items-center py-14">
+          <div className="w-1/4 divide-y-2 divide-black">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div className="flex w-full flex-col items-center">
+          <h1 className="text-4xl w-1/2 font-bold text-center text-vnuk-red pb-10">
+            All articles
+          </h1>
+        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
-          {posts.map(post => (
+          {posts.map((post) => (
             <Link key={post._id} href={`/post/${post.slug.current}`}>
-              <div className="border rounded-md drop-shadow-xl group cursor-pointer overflow-hidden">
+              <div className="drop-shadow-xl group cursor-pointer overflow-hidden">
                 <img
-                  className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  className="h-80 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                   src={urlFor(post.mainImage).url()!}
-                  alt="" />
-                <div className="flex justify-between p-5 bg-white">
+                  alt=""
+                />
+                <div className="flex justify-center p-5">
                   <div>
-                    <p className="text-2xl font-bold">{post.title}</p>
-                    <p className="text-sm">{post.description} by
-                      <span className="text-red-500 hover:text-violet-800"> {post.author.name}</span>
-                    </p>
+                    <p className="text-2xl font-light">{post.title}</p>
                   </div>
-                  <img
-                    className="h-12 w-12 rounded-full"
-                    src={urlFor(post.author.image).url()!} alt="" />
                 </div>
               </div>
             </Link>
           ))}
         </div>
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+          {posts.map((post) => (
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className="border rounded-md drop-shadow-xl group cursor-pointer overflow-hidden">
+                <img
+                  className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+                  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+                <div className="flex justify-between p-5 bg-white">
+                  <div>
+                    <p className="text-2xl font-bold">{post.title}</p>
+                    <p className="text-sm">
+                      {post.description.slice(0, 50).concat("...")} by
+                      <span className="text-red-500 hover:text-violet-800">
+                        {" "}
+                        {post.author.name}
+                      </span>
+                    </p>
+                  </div>
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                    src={urlFor(post.author.image).url()!}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div> */}
       </div>
     </MainLayout>
   );
@@ -64,5 +117,5 @@ export const getServerSideProps = async () => {
     props: {
       posts,
     },
-  }
-}
+  };
+};
