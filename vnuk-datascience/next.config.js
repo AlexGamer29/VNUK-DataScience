a735bd2ecd3451acc5b031ca1d2ffee1fcff1350
[TUF] AlexGamer29 <sun123vet@gmail.com>
2022-07-29 23:41:29 +0700

    [add] env variables to nextConfig

--- vnuk-datascience/next.config.js
+++ vnuk-datascience/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["images.wallpapersden.com"],
+  },
+  env: {
+    NEXT_PUBLIC_SANITY_DATASET: "production",
+    NEXT_PUBLIC_SANITY_PROJECT_ID: "4ypz59wh",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
